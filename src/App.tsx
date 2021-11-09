7d9843394bed0acc0f165ac2211ad9e2dedc5afb
KBeDevel <be.calisto@protonmail.com>
2021-11-09 15:53:46 -0300

    fix: re-organize imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 import { Smartblock } from 'types';
+import { Logger } from 'helpers/logger';
 import Router from 'router';
 import 'styles/_imports.sass';
 import 'styles/_global.sass';
-import { Logger } from 'helpers/logger';
 
 const App: Smartblock.Types.IsolatedComponent = () => {
   useEffect(() => {
