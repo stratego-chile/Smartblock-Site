d875e4f9243376c1e7a7caddf6f179bad63c4421
KBeDevel <theuserbit@protonmail.com>
2021-12-28 12:12:31 -0300

    feat: add encoding/hashing helper

--- src/helpers/hasher.ts
+++ src/helpers/hasher.ts
@@ -6,16 +6,18 @@ export class Hasher {
     asSHA512: (
       objectToHash: string | number[] | ArrayBuffer | Uint8Array
     ): string => SHA512.create().update(objectToHash).hex(),
-  }
+  };
+
   public static encode = {
     Base64: (
       data: WithImplicitCoercion<string> | { [Symbol.toPrimitive](hint: 'string'): string }
     ): string => Buffer.from(data).toString('base64'),
-  }
+  };
+
   public static decode = {
     Base64: (
       data: WithImplicitCoercion<string> | { [Symbol.toPrimitive](hint: 'string'): string },
       encoding: BufferEncoding = 'base64'
     ): string => Buffer.from(data, encoding).toString('ascii'),
-  }
+  };
 }
