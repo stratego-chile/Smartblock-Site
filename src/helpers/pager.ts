371035f9dffa24ddcccd75c639ba582910699b56
KBeDevel <theuserbit@protonmail.com>
2021-12-28 12:15:54 -0300

    feat: add pages helper

--- src/helpers/pager.ts
+++ src/helpers/pager.ts
@@ -1,7 +1,7 @@
-import { basePageTitle } from 'helpers/defaults';
+import { BASE_PAGE_TITLE } from 'helpers/defaults';
 
 export class Title {
-  public static set(text?: string, baseTitle = basePageTitle): void {
+  public static set(text?: string, baseTitle = BASE_PAGE_TITLE): void {
     document.title = text ? text + ' - ' + baseTitle : baseTitle;
   }
 }
