ff22d259c73de144c03f094c1b7cc8cb3b6f4443
KBeDevel <theuserbit@protonmail.com>
2021-12-28 12:21:32 -0300

    feat: add private layout sub-components

---
+++ src/components/shared/private-top-bar.tsx
@@ -0,0 +1,71 @@
+import TopBarStyles from 'styles/modules/top-bar.module.sass';
+import { useState, MouseEvent } from 'react';
+import { Badge, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Smartblock } from 'types';
+import { FiSettings, FiBell } from 'react-icons/fi';
+import NotificationsWrapper from 'components/shared/notifications-wrapper';
+import { LinkContainer } from 'react-router-bootstrap';
+import { PrivateRoutesMap } from 'routes/private.map';
+import PrivateTopBarStyles from 'styles/modules/private-top-bar.module.sass';
+import { useStyleModules } from 'helpers/props';
+
+const PrivateTopBar: Smartblock.Types.IsolatedComponent = (): JSX.Element => {
+
+  const [notifications, setNotifications] = useState<Smartblock.Types.Notification[]>([]);
+  const [showPanel, setShowPanel] = useState<boolean>(false);
+
+  const toggleNotificationsPanel = (event: MouseEvent) => {
+    if (event.isTrusted) {
+      setShowPanel(!showPanel);
+    }
+  };
+
+  return (
+    <>
+      <Navbar bg='light' className={useStyleModules(PrivateTopBarStyles.navbar, 'shadow')}>
+        <Container fluid>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link
+                className='fs-5'
+                href="#"
+                onClick={toggleNotificationsPanel}>
+                <FiBell />
+                {
+                  notifications.filter(notification => !notification.dismissed).length > 0
+                    ? <sup>
+                      <small className={TopBarStyles.notificationBadge}>
+                        <Badge pill bg='danger'>
+                          &nbsp;
+                        </Badge>
+                      </small>
+                    </sup>
+                    : null
+                }
+              </Nav.Link>
+              <NavDropdown
+                className='fs-5'
+                align='end'
+                title={<FiSettings />}
+                id="basic-nav-dropdown">
+                <NavDropdown.Item href="https://app.smartblock.cl/account">Cuenta</NavDropdown.Item>
+                <LinkContainer to={PrivateRoutesMap.preferences.path}>
+                  <NavDropdown.Item>Preferencias</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="https://app.smartblock.cl/logout">Cerrar Sesión</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <NotificationsWrapper
+        showPanel={showPanel}
+        onPanelClose={setShowPanel}
+        onNotificationsLoad={setNotifications} />
+    </>
+  );
+};
+
+export default PrivateTopBar;
---
+++ src/components/shared/side-bar.tsx
@@ -0,0 +1,107 @@
+import { FC, useState, useLayoutEffect, MouseEvent } from 'react';
+import SideBarStyles from 'styles/modules/side-bar.module.sass';
+import { Image, Button } from 'react-bootstrap';
+import { useStyleModules } from 'helpers/props';
+import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
+import { SideBarData } from 'components/utils/sidebar-links';
+import Manifest from '../../../package.json';
+import { Link } from 'react-router-dom';
+
+type SideBarModulesRefs = Record<string, boolean>;
+
+type SideBarProps = {
+  showSideBar?: boolean;
+};
+
+const SideBar: FC<SideBarProps> = (props) => {
+
+  const { showSideBar } = props;
+  
+  const [moduleRefs, setModulesRefs] = useState<SideBarModulesRefs>({});
+
+  const toggleSubModuleVisibility = (event: MouseEvent) => {
+    if (event.isTrusted) {
+      const currentIndex = (event.target as HTMLButtonElement)?.getAttribute('data-index');
+      if (currentIndex) {
+        setModulesRefs({
+          ...moduleRefs,
+          [currentIndex]: !moduleRefs[currentIndex]
+        });
+      }
+    }
+  };
+
+  useLayoutEffect(() => {
+    const newModulesRefs: SideBarModulesRefs = { };
+    SideBarData.forEach((_data, index) => {
+      Object.assign(newModulesRefs, {
+        [index]: false
+      });
+    });
+    setModulesRefs(newModulesRefs);
+  }, []);
+
+  return (
+    <div className={SideBarStyles.sideBar}>
+      <div className='d-flex flex-wrap align-items-center' style={{ height: 80 }}>
+        <Image
+          className='d-block mx-auto'
+          alt='Smartblock'
+          src='https://smartblock-static.s3.amazonaws.com/public-assets/smartblock-logo-white.svg'
+          width={247}
+          fluid />
+      </div>
+      <div className={useStyleModules(showSideBar ? '' : 'w-0', SideBarStyles.sideBarSub)}>
+        {SideBarData.map((moduleData, index) =>
+          <div key={index} className={SideBarStyles.moduleContainer} >
+            <div className={useStyleModules('d-grid gap-2', 'px-2')}>
+              <Button className={useStyleModules(SideBarStyles.togglerButton, moduleRefs[index] ? SideBarStyles.focused : SideBarStyles.base, 'nowrap')} data-index={index} onClick={toggleSubModuleVisibility}>
+                <span className='d-flex justify-content-between'>
+                  {moduleData.title}
+                  <span>{moduleRefs[index] ? <BsChevronUp /> : <BsChevronDown />}</span>
+                </span>
+              </Button>
+            </div>
+            <div className={useStyleModules(moduleRefs[index] ? 'shown' : 'hidden', SideBarStyles.linksContainer, 'mx-2') } >
+              <ul>
+                {moduleData.subModules.map((subModule, index) =>
+                  <li key={index}>
+                    <Link to={subModule.path} className={SideBarStyles.sideBarLink}>
+                      {subModule.title}
+                    </Link>
+                  </li>
+                )}
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
+      {/* padding: '0rem .5rem' */}
+      <div className={useStyleModules('w-100 px-2 py-0')} style={{ lineHeight: '.7' }}>
+        <p className={useStyleModules(SideBarStyles.pureLinksContainer, 'mb-4')}>
+          <Link
+            className={useStyleModules(SideBarStyles.pureLink)}
+            to='/about-cookies'>Acerca de las cookies</Link>
+        </p>
+        <p className={useStyleModules(SideBarStyles.pureLinksContainer, 'mb-4')}>
+          <Link
+            className={useStyleModules(SideBarStyles.pureLink)}
+            to='/terms-of-service'>Términos y condiciones del servicio</Link>
+        </p>
+        <p className={useStyleModules(SideBarStyles.pureLinksContainer, 'mb-4')}>
+          <Link
+            className={useStyleModules(SideBarStyles.pureLink)}
+            to='/privacy-policy'>Política de Privacidad</Link>
+        </p>
+      </div>
+      <div className={useStyleModules(SideBarStyles.pureLinksContainer)} style={{ height: 25, backgroundColor: '#525252', color: '#C4C4C4' }}>
+        <span className='mx-2'>
+          <span>{'Smartblock Web App'}</span>
+          <span className={useStyleModules('float-end me-2')}>{Manifest.version}</span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
