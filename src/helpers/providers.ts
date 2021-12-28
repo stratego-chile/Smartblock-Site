fa8384fbd16b9c964f1f4e810571a2171d3cc87f
KBeDevel <theuserbit@protonmail.com>
2021-12-28 12:16:04 -0300

    feat: add providers helper

--- src/helpers/providers.ts
+++ src/helpers/providers.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import { Smartblock } from 'types';
-import { apiBaseUrl } from 'helpers/defaults';
+import { API_BASE_URL } from 'helpers/defaults';
 import { Hasher } from 'helpers/hasher';
+import { BrowserStorage, Cookie } from 'browdb';
+import { SESSION_COOKIE_ACCESSOR, SESSION_DATA_ACCESSOR } from 'helpers/constants';
 
 axios.interceptors.request.use(
   (config) => {
-    const extendedHeaders = config.url?.includes(apiBaseUrl)
+    const extendedHeaders = config.url?.includes(API_BASE_URL)
       ? ProvidersHelper.getRequestHeaders()
       : ProvidersHelper.getDefaultRequestHeaders();
     config.headers = {
@@ -34,16 +36,19 @@ axios.interceptors.response.use(
   }
 );
 
+const SESSION_COOKIE_REF = Hasher.encode.Base64(SESSION_COOKIE_ACCESSOR);
+const SESSION_DATA_REF = Hasher.encode.Base64(SESSION_DATA_ACCESSOR);
+
 export class ProvidersHelper {
   public static HttpClient = axios;
 
   public static readonly LocalStorage = {
-    sessionData: 'session'
-  }
+    sessionData: SESSION_DATA_REF
+  };
 
   public static readonly keys = {
     api: {
-      publicKey: process.env.REACT_APP_KEY_API_PUBLIC_TOKEN ?? '',
+      publicKey: process.env.REACT_APP_KEY_API_PUBLIC_TOKEN,
       privateKey: ProvidersHelper.getPrivateToken
     },
     hubspot: {
@@ -51,20 +56,20 @@ export class ProvidersHelper {
     },
     google: {
       places: {
-        autocomplete: process.env.REACT_APP_KEY_PLACES_AUTOCOMPLETE_PUBLIC_TOKEN ?? '',
-        details: process.env.REACT_APP_KEY_PLACES_AUTOCOMPLETE_PUBLIC_TOKEN ?? ''
+        autocomplete: process.env.REACT_APP_KEY_PLACES_AUTOCOMPLETE_PUBLIC_TOKEN,
+        details: process.env.REACT_APP_KEY_PLACES_AUTOCOMPLETE_PUBLIC_TOKEN
       },
       recaptcha: {
-        v2: process.env.REACT_APP_KEY_GRECAPTCHA_V2_PUBLIC_TOKEN ?? '',
-        v3: process.env.REACT_APP_KEY_GRECAPTCHA_V3_PUBLIC_TOKEN ?? ''
+        v2: process.env.REACT_APP_KEY_GRECAPTCHA_V2_PUBLIC_TOKEN,
+        v3: process.env.REACT_APP_KEY_GRECAPTCHA_V3_PUBLIC_TOKEN
       }
     }
-  }
+  };
 
   private static _baseHeaders = {
     'Content-Type': 'application/json; charset=utf-8',
     Accept: 'application/json',
-  }
+  };
 
   public static getRequestHeaders(): Smartblock.Types.DefaultRequestHeaders {
     return {
@@ -76,6 +81,17 @@ export class ProvidersHelper {
 
   public static getDefaultRequestHeaders(): (typeof ProvidersHelper._baseHeaders) {
     return ProvidersHelper._baseHeaders;
+  }
+
+  public static getSession (): Smartblock.Types.Session {
+    const sessionCookieRegistered = Cookie.isRegistered(SESSION_COOKIE_REF);
+    const sessionCookie = Cookie.obtain(SESSION_COOKIE_REF);
+    const sessionData = BrowserStorage.obtainItem<Smartblock.Types.SessionData>(SESSION_DATA_REF);
+
+    return {
+      cookie: sessionCookieRegistered ? sessionCookie : null,
+      data: sessionData instanceof Object ? { ...sessionData } : sessionData
+    };
   }
 
   public static getPrivateToken(): string | null {
