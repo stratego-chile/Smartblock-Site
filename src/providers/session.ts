3780f4bf240f3974ef1f524d1875b35d09e861a5
KBeDevel <theuserbit@protonmail.com>
2021-12-28 12:11:18 -0300

    feat: add session provider

--- src/providers/session.ts
+++ src/providers/session.ts
@@ -1,5 +1,12 @@
+import { ProvidersHelper } from 'helpers/providers';
+import { Smartblock } from 'types';
+
 export class SessionProvider {
-  public static isAuthenticated(): boolean {
-    return false;
+  public static isAuthenticated (): boolean {
+    return !!SessionProvider.getSession().data;
+  }
+
+  public static getSession (): Smartblock.Types.Session {
+    return ProvidersHelper.getSession();
   }
 }
