25c61a242c14e20725e72f34665830333e0e28de
KBeDevel <theuserbit@protonmail.com>
2021-12-28 12:06:01 -0300

    feat: Add REACT_APP_RECAPTCHA_V3_KEY to environment spec

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       REACT_APP_DEPLOY_PLATFORM: 'gh-pages' | 'code-deploy';
+      REACT_APP_RECAPTCHA_V3_KEY: string;
     }
   }
 }
