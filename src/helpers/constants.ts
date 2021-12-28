9a954180e303eaa8fd91d428beeb8565068f9867
KBeDevel <theuserbit@protonmail.com>
2021-12-28 12:11:50 -0300

    feat: add app constants helper

---
+++ src/helpers/constants.ts
@@ -0,0 +1,7 @@
+export const EMPTY_STRING = String();
+
+export const SESSION_COOKIE_ACCESSOR = 'sb_session';
+
+export const SESSION_TOKEN_COOKIE_ACCESSOR = 'sb_session_token';
+
+export const SESSION_DATA_ACCESSOR = 'sb_session_data';
