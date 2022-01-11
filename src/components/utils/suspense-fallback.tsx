7240b54b5074ccdbeae4570bf26c0d51825145fc
KBeDevel <theuserbit@protonmail.com>
2022-01-10 21:57:30 -0300

    fix: loader spinner implementation in suspense callback meta-component

--- src/components/utils/suspense-fallback.tsx
+++ src/components/utils/suspense-fallback.tsx
@@ -1,16 +1,19 @@
 import { Container, Row, Col } from 'react-bootstrap';
-import Loader from 'react-loader-spinner';
+import { BallTriangle } from 'react-loader-spinner';
 import SplashScreenStyles from 'styles/modules/splash-screen.module.sass';
 
 const SuspenseFallback = (descriptorText?: string): JSX.Element => {
   return (
     <Container className={descriptorText ? SplashScreenStyles.innerWrapper : SplashScreenStyles.wrapper}>
-      <Row className="h-100 align-content-center">
-        <Col className="text-center">
-          <Loader
-            type="BallTriangle"
-            color="#5a5a5a" />
-          {descriptorText ? <h5 className="mt-5">{descriptorText}</h5> : null}
+      <Row className='h-100 align-content-center'>
+        <Col>
+          <BallTriangle
+            wrapperClass='w-100 justify-content-center'
+            arialLabel='loading'
+            height='100'
+            width='100'
+            color='#5A5A5A' />
+          {descriptorText ? <h5 className='mt-5'>{descriptorText}</h5> : null}
         </Col>
       </Row>
     </Container>
